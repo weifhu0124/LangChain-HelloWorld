7e111e8ed3e6491bb2303be20a1e024bbd13276d
Weifeng Hu <weifhu@umich.edu>
2025-07-26 21:33:07 -0700

    Create linkedin lookup agent

--- ice_breaker/agents/linkedin_lookup_agent.py
+++ ice_breaker/agents/linkedin_lookup_agent.py
@@ -5,35 +5,33 @@ from langchain.agents import (
     create_react_agent,
     AgentExecutor,
 )
-from langchain_core.tools import Tool
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
-from tools.tools import get_profile_url_tavily
-
-load_dotenv()
+from langchain_tavily import TavilySearch
 
 
 def lookup(name: str) -> str:
     llm = ChatOpenAI(
         temperature=0,
-        model_name="gpt-4o-mini",
-        openai_api_key=os.environ["OPENAI_API_KEY"],
+        model="gpt-3.5-turbo",
     )
     template = """given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
-                          Your answer should contain only a URL"""
+                          Your answer should contain only a URL starting with https"""
 
     prompt_template = PromptTemplate(
         template=template, input_variables=["name_of_person"]
     )
     tools_for_agent = [
-        Tool(
-            name="Crawl Google 4 linkedin profile page",
-            func=get_profile_url_tavily,
-            description="useful for when you need get the Linkedin Page URL",
+        # Initialize Tavily Search Tool
+        TavilySearch(
+            max_results=5,
+            topic="general",
+            name="Search Tavily for LinkedIn Profile"
         )
     ]
 
+    # https://smith.langchain.com/hub/hwchase17/react
     react_prompt = hub.pull("hwchase17/react")
     agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
@@ -44,3 +42,8 @@ def lookup(name: str) -> str:
 
     linked_profile_url = result["output"]
     return linked_profile_url
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    lookup("Weifeng Hu AWS")
