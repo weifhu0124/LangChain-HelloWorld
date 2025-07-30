9c4654bd8474557ce52c3d1b467188451cca1bf4
Weifeng Hu <weifhu@umich.edu>
2025-07-30 10:41:08 -0700

    Use rephrase prompt for memory update

--- documentation_helper/backend/retrieval_augmentation.py
+++ documentation_helper/backend/retrieval_augmentation.py
@@ -1,8 +1,10 @@
 import os
+from typing import Tuple, List
 
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
@@ -10,7 +12,7 @@ from langchain_pinecone import PineconeVectorStore
 load_dotenv()
 
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: List[Tuple[str, str]]):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     docsearch = PineconeVectorStore(index_name=os.environ["INDEX_NAME"], embedding=embeddings)
     chat = ChatOpenAI(temperature=0)
@@ -18,16 +20,21 @@ def run_llm(query: str):
     retrieval_qa_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_prompt)
 
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
+    )
+
     qa = create_retrieval_chain(
-        retriever=docsearch.as_retriever(),
+        retriever=history_aware_retriever,
         combine_docs_chain=stuff_documents_chain
     )
 
-    result = qa.invoke({"input": query})
+    result = qa.invoke({"input": query, "chat_history": chat_history})
     transformed_result = {
         "query": result["input"],
         "result": result["answer"],
-        "source_document": result["context"]
+        "source_documents": result["context"]
     }
     return transformed_result
 
